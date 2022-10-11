7d78c3cab9c25d9bd77bd46cd859730ec7d9d220
dudu1590 <115583025+dudu1590@users.noreply.github.com>
2022-10-12 04:29:55 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oVsC45tQQxcaaJ9NZ7F4kVPrAEkw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'diJvL_PVjqLRJxUHJDUBdp_jp0VFQfksbIscV6RViHY',
+      useTemplateId: 'cetbzuAGZWcbkHCDQ-T6-4hBM2sQEloW8pRhhr4AXBE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '05-06',
       festivals: [
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '宝宝', year: '2002', date: '05-06',
+          type: '生日', name: '宝贝', year: '2002', date: '05-06',
         },
         {
           type: '节日', name: '相识纪念日', year: '2020', date: '08-06',
@@ -43,15 +43,13 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2020-08-06' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-10-10' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'diJvL_PVjqLRJxUHJDUBdp_jp0VFQfksbIscV6RViHY',
+  CALLBACK_TEMPLATE_ID: 'cetbzuAGZWcbkHCDQ-T6-4hBM2sQEloW8pRhhr4AXBE',
 
   CALLBACK_USERS: [
     {
